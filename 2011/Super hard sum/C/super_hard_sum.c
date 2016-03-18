33714a05c659a8bcdf82d09ead7c28029fc29b5e
Jaime <jaume_1499@hotmail.com>
2016-03-18 20:04:09 +0100

    super hard sum v0.2

--- 2011/Super hard sum/C/super_hard_sum.c
+++ 2011/Super hard sum/C/super_hard_sum.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int get_num(char*);
+//Función que consigue el primer número de un String, ej: String: 10 20 30
+//Nos devuelve 10 de tipo integer
+int get__first_num(char*);
 
 
 int main(){
@@ -10,17 +11,26 @@ int main(){
 	char *linea;
 	int resultado;
 	char *ptr;
-	int contador;
+	int acumulador;
 
+	//Bucle infinito
 	do{
 
-		resultado = 0;
-		contador = 1;
+		resultado = 0; //Cada nueva linea, el resultado se 'vacia'
+		//El acumulador tiene dos funciones:
+		// - Guarda el valor numerico que nos devuelve la función "get_num" para
+		//	 después añadirlo al resultado actual.
+		// - Si el acumulador queda a 0, significa que no hay más numeros y por
+		//	 lo tanto se ha acabado la operación.
+		acumulador = 1; 
+		//Conseguimos nuestra entrada por teclado
 		fgets(linea, 1024, stdin);
 		
-	
-		while (contador = get_num(linea)){
+		//Conseguimos el resultado y lo almacenamos en un acumulador, para
+		//después introducirlo en el resultado final
+		while (acumulador = get_first_num(linea)){
 
+			//Chapuza, para avanzar al siguiente numero
 			while(*linea != 32 && *linea != '\0' && *linea != 10){
 				linea++;
 			}
@@ -28,40 +38,49 @@ int main(){
 				linea++;
 			}
 
-
-			resultado += contador;
+			
+			resultado += acumulador;
 		}
+
+		//Una vez acabada la operación de la linea, imprimimos el resultado
 		printf("%d\n", resultado);
 
 	}while(1);
 	return 0;
 }
 
-int get_num(char *linea){
+
+//Tra
+int get_first_num(char *linea){
 	const char posibles_entradas[] = "0123456789-";
 	int resultado = 0;
 	int negativo = 0;
 	int i;
 
+	
 	if (*linea == 10 || *linea == 13 || *linea == 32 || *linea == '\0'){
 		return 0;
 	}
 
+	//Transformar string a número
 	while (*linea != 10 && *linea != 32 && *linea != 13 && *linea != '\0'){
 		for(i = 0; posibles_entradas[i] != *linea; i++);
 		
+		linea++;
+
+		//Posible valor negativo
 		if (posibles_entradas[i] == '-'){
 			negativo = 1;
 		}else{
 			
 			resultado += i;
-			resultado *= 10;
+			//Si es el último elemento no será multiplicado por 10
+			if (*linea != 10 && *linea != 32 && *linea != 13 && *linea != '\0'){
+				resultado *= 10;
+			}
+		
 		}
-		linea++;
 	}
-	//Último elemento 
-	resultado /= 10;
-	
 
 	return negativo ? -resultado : resultado;
 }
